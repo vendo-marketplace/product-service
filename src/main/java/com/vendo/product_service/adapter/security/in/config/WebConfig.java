f684cc12fa80b3cb497f77ccdbd3a65aed791de4
HaaaamBurger <gemasclashes@gmail.com>
2026-03-24 20:21:10 +0200

    cors, swagger ui

---
+++ src/main/java/com/vendo/product_service/adapter/security/in/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.vendo.product_service.adapter.security.in.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${server.url}")
+    private String SERVER_URL;
+
+    @Value("${client.url}")
+    private String CLIENT_URL;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "https://*.vercel.app"
+                )
+                .allowedOrigins(SERVER_URL, CLIENT_URL)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
