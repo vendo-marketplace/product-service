fdba18b804c6c4ed32b70410ecc8400f52ecf717
HaaaamBurger <gemasclashes@gmail.com>
2026-03-27 22:21:38 +0200

    logging

--- src/main/java/com/vendo/product_service/adapter/security/in/JwtAuthFilter.java
+++ src/main/java/com/vendo/product_service/adapter/security/in/JwtAuthFilter.java
@@ -39,7 +39,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         SecurityContext securityContext = SecurityContextHolder.getContext();
-        System.out.println("Context: " + securityContext);
         if (securityContext.getAuthentication() != null) {
             filterChain.doFilter(request, response);
             return;
@@ -47,14 +46,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         try {
             String jwtToken = getTokenFromRequest(request);
-            System.out.println("Token: " + jwtToken);
             TokenClaims claims = claimsParser.extract(jwtToken);
-            System.out.println("Claims: " + claims);
             UserActivityValidator.validate(claims.status(), claims.emailVerification());
-            System.out.println("Validated user.");
 
             addAuthenticationToContext(claims.userId(), claims.roles());
         } catch (Exception e) {
+            log.error(e.getMessage());
             handlerExceptionResolver.resolveException(request, response, null, e);
             return;
         }
