da3bec0e30b48ed89ee6609a9c08793bb5ea7a7d
HaaaamBurger <gemasclashes@gmail.com>
2026-05-02 20:04:44 +0300

    #kan-19: Unauthorized bug fix

--- src/main/java/com/vendo/product_service/adapter/security/out/SecurityContextHelper.java
+++ src/main/java/com/vendo/product_service/adapter/security/out/SecurityContextHelper.java
@@ -1,5 +1,6 @@
 package com.vendo.product_service.adapter.security.out;
 
+import com.vendo.product_service.adapter.security.out.jwt.parser.TokenClaims;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,10 +10,10 @@ public class SecurityContextHelper {
     public static String getUserIdFromContext() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || !(authentication.getPrincipal() instanceof String userId)) {
+        if (authentication == null || !(authentication.getPrincipal() instanceof TokenClaims claims)) {
             throw new AuthenticationCredentialsNotFoundException("Unauthorized.");
         }
 
-        return userId;
+        return claims.userId();
     }
 }
