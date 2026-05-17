8a67ec5172f64e06484ef5e5867505f1709d0548
HaaaamBurger <gemasclashes@gmail.com>
2026-05-17 11:20:41 +0300

    kan-25: logging

--- src/main/java/com/vendo/product_service/adapter/product/out/persistence/InternalProductQueryAdapter.java
+++ src/main/java/com/vendo/product_service/adapter/product/out/persistence/InternalProductQueryAdapter.java
@@ -4,6 +4,7 @@ import com.vendo.product_service.adapter.product.out.mapper.MongoProductMapper;
 import com.vendo.product_service.domain.product.model.Product;
 import com.vendo.product_service.port.out.product.InternalProductQueryPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Limit;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class InternalProductQueryAdapter implements InternalProductQueryPort {
@@ -20,11 +22,15 @@ public class InternalProductQueryAdapter implements InternalProductQueryPort {
 
     @Override
     public List<Product> getAll(Instant cursor, int limit) {
+        log.info("Logging input: {}, {}.", cursor, limit);
         if (Objects.isNull(cursor)) {
-            return mapper.toProducts(repository.findAllByOrderByCreatedAtDesc(Limit.of(limit)));
+            List<Product> products = mapper.toProducts(repository.findAllByOrderByCreatedAtDesc(Limit.of(limit)));
+            log.info(products.toString());
+            return products;
         }
 
         List<MongoProduct> cursorProducts = repository.getAllByCreatedAtOrderByCreatedAtDesc(cursor, Limit.of(limit));
+        log.info(cursorProducts.toString());
         return mapper.toProducts(cursorProducts);
     }
 }
