11717ff4f2d1ef99b668583572a73f1f22d9d01f
HaaaamBurger <gemasclashes@gmail.com>
2026-05-19 19:18:28 +0300

    kan-30: mapping createdAt for internal getAll

--- src/main/java/com/vendo/product_service/adapter/product/in/dto/ProductResponse.java
+++ src/main/java/com/vendo/product_service/adapter/product/in/dto/ProductResponse.java
@@ -4,6 +4,7 @@ import com.vendo.product_service.domain.attribute.model.AttributeValue;
 import lombok.Builder;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
@@ -16,6 +17,7 @@ public record ProductResponse (
         String ownerId,
         String categoryId,
         List<AttributeValue> attributes,
-        boolean active
+        boolean active,
+        LocalDateTime createdAt
 ) {
 }
