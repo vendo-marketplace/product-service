e4462431aaa3fe45073b3531918d37a61a005dc2
HaaaamBurger <gemasclashes@gmail.com>
2026-06-11 10:43:39 +0300

    kan-43: spring exceptions handling

--- src/main/java/com/vendo/product_service/adapter/spring/exception/SpringExceptionHandler.java
+++ src/main/java/com/vendo/product_service/adapter/spring/exception/SpringExceptionHandler.java
@@ -4,77 +4,99 @@ import com.vendo.security_lib.exception.response.ExceptionResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
-public class SpringExceptionHandler {
+public class SpringExceptionHandler extends ResponseEntityExceptionHandler {
 
     private final ValidationErrorConverter validationErrorConverter;
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
-        Map<String, String> errors = validationErrorConverter.fromField(e.getBindingResult().getFieldErrors());
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        Map<String, String> errors = validationErrorConverter.fromField(ex.getBindingResult().getFieldErrors());
 
-        ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+        ExceptionResponse response = ExceptionResponse.builder()
                 .message("Validation failed.")
                 .errors(errors)
                 .code(HttpStatus.BAD_REQUEST.value())
-                .path(request.getRequestURI())
+                .path(path)
                 .build();
 
-        return ResponseEntity.badRequest().body(exceptionResponse);
+        return ResponseEntity.status(status.value()).body(response);
     }
 
-    @ExceptionHandler(HandlerMethodValidationException.class)
-    protected ResponseEntity<ExceptionResponse> handleHandlerMethodValidationException(HandlerMethodValidationException e, HttpServletRequest request) {
-        Map<String, String> errors = validationErrorConverter.fromParameter(e.getParameterValidationResults());
+    @Override
+    protected ResponseEntity<Object> handleHandlerMethodValidationException(HandlerMethodValidationException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        Map<String, String> errors = validationErrorConverter.fromParameter(ex.getParameterValidationResults());
 
-        ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+        ExceptionResponse response = ExceptionResponse.builder()
                 .message("Validation failed.")
                 .code(HttpStatus.BAD_REQUEST.value())
                 .errors(errors)
-                .path(request.getRequestURI())
+                .path(path)
                 .build();
 
-        return ResponseEntity.badRequest().body(exceptionResponse);
+        return ResponseEntity.badRequest().body(response);
     }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    protected ResponseEntity<ExceptionResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e, HttpServletRequest request) {
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
         ExceptionResponse exceptionResponse = ExceptionResponse.builder()
                 .message("Unsupported media type.")
                 .code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
-                .path(request.getRequestURI())
+                .path(path)
                 .build();
 
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(exceptionResponse);
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    protected ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest request) {
-        log.error(e.getMessage());
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.error(ex.getMessage());
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
         ExceptionResponse exceptionResponse = ExceptionResponse.builder()
                 .message("Invalid body structure.")
                 .code(HttpStatus.BAD_REQUEST.value())
-                .path(request.getRequestURI())
+                .path(path)
                 .build();
 
         return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
-    @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class})
-    protected ResponseEntity<ExceptionResponse> handleCommonExceptions(Exception e, HttpServletRequest request) {
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+        ExceptionResponse response = ExceptionResponse.builder()
+                .message("Method not allowed.")
+                .code(HttpStatus.METHOD_NOT_ALLOWED.value())
+                .path(path)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED.value()).body(response);
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class, NullPointerException.class})
+    protected ResponseEntity<ExceptionResponse> handleException(Exception e, HttpServletRequest request) {
         log.error(e.getMessage());
         ExceptionResponse exceptionResponse = ExceptionResponse.builder()
                 .message("Internal server error.")
