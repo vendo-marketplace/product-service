4c7fbc18aa78f364f987d2a2485a513663100e82
rel.ffi <kronbergdron@gmail.com>
2026-06-24 09:57:54 +0300

    add handler to FavoriteAlreadyExistsException

---
+++ src/main/java/com/vendo/product_service/adapter/favorite/in/exception/FavoriteExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.vendo.product_service.adapter.favorite.in.exception;
+
+import com.vendo.product_service.domain.favorite.exception.FavoriteAlreadyExistsException;
+import com.vendo.security_lib.exception.response.ExceptionResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+class FavoriteExceptionHandler {
+
+    @ExceptionHandler(FavoriteAlreadyExistsException.class)
+    ResponseEntity<ExceptionResponse> handleFavoriteAlreadyExistsException(FavoriteAlreadyExistsException e, HttpServletRequest request) {
+        ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .message(e.getMessage())
+                .code(HttpStatus.CONFLICT.value())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionResponse);
+    }
+
+}
