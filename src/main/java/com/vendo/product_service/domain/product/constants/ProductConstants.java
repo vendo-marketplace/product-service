1f79068f553ad3c0aed98048c2e86348b3fe8e47
HaaaamBurger <gemasclashes@gmail.com>
2026-06-27 10:57:35 +0300

    fixing slug pattern

--- src/main/java/com/vendo/product_service/domain/product/constants/ProductConstants.java
+++ src/main/java/com/vendo/product_service/domain/product/constants/ProductConstants.java
@@ -2,7 +2,7 @@ package com.vendo.product_service.domain.product.constants;
 
 public final class ProductConstants {
 
-    public static final String SLUG_PATTERN = "^[a-z0-9]+(?:-[a-z0-9]+)*$";
+    public static final String SLUG_PATTERN = "^[a-z0-9]+(?:_[a-z0-9]+)*$";
 
     private ProductConstants() {}
 
