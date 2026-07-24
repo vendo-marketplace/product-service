4e4ea0a9a69ceefba2a1a51c750bea29423783a1
HaaaamBurger <gemasclashes@gmail.com>
2026-07-24 17:08:37 +0300

    logging exception file size exceeded

--- src/main/java/com/vendo/product_service/adapter/TestExceptionHandler.java
+++ src/main/java/com/vendo/product_service/adapter/TestExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -14,12 +17,19 @@ public class TestExceptionHandler {
 
     @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class, NullPointerException.class})
     protected ResponseEntity<ExceptionResponse> handleException(Exception e, HttpServletRequest request) {
-        log.error(String.valueOf(e));
-        log.error("Handling internal exception: {}.", e.getMessage());
-        log.error(String.valueOf(e.getCause()));
-        log.error(String.valueOf(e.getCause().getClass()));
-        log.error(e.getClass().getName());
         ExceptionResponse exceptionResponse = ExceptionResponse.builder().message("Internal server error.").code(HttpStatus.INTERNAL_SERVER_ERROR.value()).path(request.getRequestURI()).build();
+        return ResponseEntity.internalServerError().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    protected ResponseEntity<ExceptionResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e, HttpServletRequest request) {
+        log.info(String.valueOf(e));
+        ExceptionResponse exceptionResponse = ExceptionResponse.builder()
+                .message("File size upload exceeded.")
+                .code(HttpStatus.BAD_REQUEST.value())
+                .errors(Map.of(e.getBody().getTitle(), e.getBody().getDetail()))
+                .path(request.getRequestURI())
+                .build();
         return ResponseEntity.internalServerError().body(exceptionResponse);
     }
 
