ed9bab3ac7496182cd7c7ab005f37502d4b8d61d
HaaaamBurger <gemasclashes@gmail.com>
2026-07-27 14:53:40 +0300

    category title pattern fix

--- src/main/java/com/vendo/product_service/domain/product/pattern/ProductPatterns.java
+++ src/main/java/com/vendo/product_service/domain/product/pattern/ProductPatterns.java
@@ -2,8 +2,8 @@ package com.vendo.product_service.domain.product.pattern;
 
 public final class ProductPatterns {
 
-    public static final String TITLE_PATTERN = "^[A-ZА-ЯІЇЄ][a-zа-яіїє]+(?:['’][A-Za-zА-ЯІЇЄа-яіїє]+)*(?: [A-Za-zА-ЯІЇЄа-яіїє][a-zа-яіїє]*(?:['’][A-Za-zА-ЯІЇЄа-яіїє]+)*)?$";
-    public static final String TITLE_VALIDATION_MESSAGE = "Title must start with a capital letter and may contain only letters, spaces, hyphens, commas, and apostrophes.";
+    public static final String TITLE_PATTERN = "^[A-ZА-ЯІЇЄ][A-Za-zА-Яа-яІіЇїЄє0-9'’/,-]*(?: [A-Za-zА-Яа-яІіЇїЄє0-9'’/,-]+)*$";
+    public static final String TITLE_VALIDATION_MESSAGE = "Title must start with a capital letter and may contain only letters, spaces, commas, slashes, hyphens, and apostrophes.";
 
     public static final String SLUG_PATTERN = "[a-z0-9]+(?:_[a-z0-9]+)*$";
     public static final String SLUG_VALIDATION_MESSAGE = "Slug may contain only lowercase letters, numbers, and underscores, and cannot start or end with an underscore.";
