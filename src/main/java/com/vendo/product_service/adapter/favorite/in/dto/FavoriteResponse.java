f39cfd253ad80bfb79494f527cc2dc4173105ace
HaaaamBurger <gemasclashes@gmail.com>
2026-07-30 11:35:14 +0300

    kan-97: add address to favorites response

--- src/main/java/com/vendo/product_service/adapter/favorite/in/dto/FavoriteResponse.java
+++ src/main/java/com/vendo/product_service/adapter/favorite/in/dto/FavoriteResponse.java
@@ -1,12 +1,17 @@
 package com.vendo.product_service.adapter.favorite.in.dto;
 
+import com.vendo.product_service.domain.product.nested.Address;
+
 import java.math.BigDecimal;
 
 public record FavoriteResponse(
         String id,
+
         String title,
         BigDecimal price,
         Integer quantity,
+        Address address,
+
         Boolean isNew,
         Boolean active
 ) {}
